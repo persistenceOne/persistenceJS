6187a388e72c31b844828f0ec681676a5c23c2fe
Aditi  Maheshwari <aditimaheshwari121@gmail.com>
2020-11-03 19:29:19 +0530

    Added Promise.

--- init.js
+++ init.js
@@ -5,12 +5,14 @@ const file = require("./config.json");
 function init(url){
 
     file.lcdURL = url;
-
-    fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
-        if (err) return console.log(err);
-        console.log(JSON.stringify(file));
-        console.log('writing to ' + fileName);
-    });
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
+            if (err) return console.log(err);
+            console.log(JSON.stringify(file));
+            console.log('writing to ' + fileName);
+            resolve("success")
+        });
+    })
 }
 
 module.exports = {
