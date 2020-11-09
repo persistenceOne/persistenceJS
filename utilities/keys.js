3946bafe5bf66a3e5f040c8aa912080466ef7272
Aditi  Maheshwari <aditimaheshwari121@gmail.com>
2020-11-09 14:42:22 +0530

    Read and write file removed.

--- utilities/keys.js
+++ utilities/keys.js
@@ -1,6 +1,5 @@
 const bip39 = require("bip39");
 const bip32 = require("bip32");
-const fs = require("fs");
 const tmSig = require("@tendermint/sig");
 const config = require("../config.json");
 const crypto = require("crypto");
@@ -41,7 +40,7 @@ function getWalletPath() {
     return "m/44'/118'/0'/0/0"
 }
 
-function createStore(mnemonic, password, filename = "key") {
+function createStore(mnemonic, password) {
     try {
         const key = crypto.randomBytes(32);
         const iv = crypto.randomBytes(16);
@@ -49,17 +48,14 @@ function createStore(mnemonic, password, filename = "key") {
         let encrypted = cipher.update(mnemonic);
         encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-        fs.writeFileSync(
-            filename + ".json",
-            JSON.stringify({
-                hashpwd: crypto.createHash(passwordHashAlgorithm).update(password).digest("hex"),
-                iv: iv.toString("hex"),
-                salt: key.toString("hex"),
-                crypted: encrypted.toString("hex"),
-            })
-        );
+        let obj ={
+            "hashpwd": crypto.createHash(passwordHashAlgorithm).update(password).digest("hex"),
+            "iv": iv.toString("hex"),
+            "salt": key.toString("hex"),
+            "crypted": encrypted.toString("hex")
+        }
         return {
-            success: true
+            Response: obj
         };
     } catch (exception) {
         return {
@@ -69,17 +65,17 @@ function createStore(mnemonic, password, filename = "key") {
     }
 }
 
-function decryptStore(file, password) {
-    const data = fs.readFileSync(file, { encoding: "utf8", flag: "r" });
+function decryptStore(hashpwd, iv, salt, crypted, password) {
+
     if (
-        JSON.parse(data).hashpwd === crypto.createHash(passwordHashAlgorithm).update(password).digest("hex")
+        JSON.parse(hashpwd) === crypto.createHash(passwordHashAlgorithm).update(password).digest("hex")
     ) {
-        let iv = Buffer.from(JSON.parse(data).iv, "hex");
-        let encryptedText = Buffer.from(JSON.parse(data).crypted, "hex");
+        let iv = Buffer.from(JSON.parse(iv), "hex");
+        let encryptedText = Buffer.from(JSON.parse(crypted), "hex");
 
         let decipher = crypto.createDecipheriv(
             "aes-256-cbc",
-            Buffer.from(JSON.parse(data).salt, "hex"),
+            Buffer.from(JSON.parse(salt), "hex"),
             iv
         );
         let decrypted = decipher.update(encryptedText);
