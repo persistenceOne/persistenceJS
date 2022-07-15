c233de9eafc12dda745857509563b005e063340d
vishrut <vsinghv5t@gmail.com>
2022-07-15 18:57:22 +0400

    initial commit

---
+++ scripts/telescope.ts
@@ -0,0 +1,24 @@
+import { join } from "path";
+import telescope from "@osmonauts/telescope";
+import {generate} from "cosmwasm-typescript-gen"
+
+const protoDirs = [
+    join(__dirname, ".././proto"),
+];
+const outPath = join(__dirname, ".././src");
+
+
+generate(
+    "cw20-base",
+    [join(__dirname, ".././source/cw-plus/contracts/cw20-base/schema/")],
+    join(__dirname, ".././src/contracts")
+)
+
+telescope({
+    protoDirs,
+    outPath,
+    options: {
+        includeAminos: true,
+        includeLCDClient: true,
+    }
+});
