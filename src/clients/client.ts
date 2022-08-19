6f73bea4aca6ad27fafffcfc998cf33d028db5ae
vishrut <vsinghv5t@gmail.com>
2022-08-19 10:28:11 +0400

    add optional signer

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -20,6 +20,7 @@ export interface WalletOptoions {
 
 export class PersistenceClient {
   public wallet: DirectSecp256k1HdWallet;
+  public signer
   public mnemonic: string;
   public config: Config;
   public core: SigningStargateClient;
@@ -29,6 +30,7 @@ export class PersistenceClient {
   private constructor(
     mnemonic: string,
     wallet: DirectSecp256k1HdWallet,
+    signer,
     wasm: SigningCosmWasmClient,
     core: SigningStargateClient,
     queryclient,
@@ -37,23 +39,25 @@ export class PersistenceClient {
     this.mnemonic = mnemonic;
     this.config = config;
     this.wallet = wallet;
+    this.signer = signer;
     this.core = core;
     this.wasm = wasm;
     this.query = queryclient;
   }
 
-  static async init(mnemonic: string, chainConfig?: Config): Promise<PersistenceClient> {
+  static async init(mnemonic: string, signer, chainConfig?: Config): Promise<PersistenceClient> {
     //wallet
     const config = chainConfig || LocalConfig;
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, DefaultWalletOptoions);
+    const clientsigner = signer || wallet
     //signing clients
-    const core = await SigningStargateClient.connectWithSigner(config.rpc, wallet);
-    const wasm = await SigningCosmWasmClient.connectWithSigner(config.rpc, wallet);
+    const core = await SigningStargateClient.connectWithSigner(config.rpc, clientsigner);
+    const wasm = await SigningCosmWasmClient.connectWithSigner(config.rpc, clientsigner);
     //query client
     const tendermintClient = await Tendermint34Client.connect(config.rpc);
     const queryClient = new QueryClient(tendermintClient);
     const rpc = await createProtobufRpcClient(queryClient);
     const query = await createRPCQueryClient({ rpc: rpc });
-    return new PersistenceClient(mnemonic, wallet, wasm, core, query);
+    return new PersistenceClient(mnemonic, wallet, clientsigner, wasm, core, query, config);
   }
 }
