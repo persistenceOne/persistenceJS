46abaaf1ebc562c2bf3e8bc5e2d9d9de33d8beb5
puneetmahajan <puneet@persistence.one>
2023-06-12 17:54:25 +0530

    update script

--- scripts/prepare-publishing.sh
+++ scripts/prepare-publishing.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-DIRS="confio cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint pstake persistence"
+DIRS="amino cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint pstake persistence"
 FILES="helpers.d.ts helpers.js helpers.js.map index.d.ts index.js index.js.map"
 
 for dir in $DIRS; do
